a126d8cb1dd2fce13e6fe594186a3e5553482ca2
RafaelCavallin <rafael_cavallin@hotmail.com>
2019-01-24 01:20:17 -0200

    Primeiro commit do projeto.

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php 
+	// DB params
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'shareposts');
+	// App ROOT
+	define('APPROOT', dirname(dirname(__FILE__)));
+	//URL ROOT
+	define('URLROOT', 'http://localhost/shareposts');
+	// Site Name
+	define('SITENAME', 'SharePosts');
+	// App Version
+	define('APPVERSION', '1.0.0');
---
+++ app/views/inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php echo SITENAME; ?></title>
+	<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+	<?php require APPROOT . '/views/inc/navbar.php'; ?>
+<div class="container">
